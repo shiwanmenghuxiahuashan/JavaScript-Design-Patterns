c20555d5dbe1d3d7989c8f7d2e7291604b20fade
DESKTOP-K9N66NF\李重楼 <750603136@QQ.COM>
2019-05-24 19:19:49 +0800

    新的修改
    新的修改

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
-function object(o) {
-    function F() {};
-    F.prototype = o;
-    return new F();
-}
+var StaticClass = function(){};
+StaticClass.staicVariable = "staicVariable";
+StaticClass.staicMethod = function(){
+    console.log('模拟一下');
+};
+
+console.log(StaticClass.name);      //'staicVariable'
+StaticClass.staicMethod(1,3);  //'模拟一下'
