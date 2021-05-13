cfe246319eec448c4294b461fafe2b4b8503925a
lilei <guizhong_10000@163.com>
2021-05-13 11:30:10 +0800

    根据设计模式所属类型进行划分

--- test.js
+++
@@ -1,8 +0,0 @@
-var StaticClass = function(){};
-StaticClass.staicVariable = "staicVariable";
-StaticClass.staicMethod = function(){
-    console.log('模拟一下');
-};
-
-console.log(StaticClass.name);      //'staicVariable'
-StaticClass.staicMethod(1,3);  //'模拟一下'
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$data=$_GET['data'];
-$callback=$_GET['callback'];
-
-echo $callback."('success','".$data."')";
-?>
